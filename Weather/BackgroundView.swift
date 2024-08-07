adca26ec472c9b240fb92a494c942a80fa8a48c2
Ramon Xavier <ramon1234rxm@gmail.com>
2024-08-06 22:52:18 -0300

    feat: create background view

---
+++ Weather/BackgroundView.swift
@@ -0,0 +1,23 @@
+//
+//  BackgroundView.swift
+//  Weather
+//
+//  Created by Ramon Xavier on 06/08/24.
+//
+
+import SwiftUI
+
+struct BackgroundView: View {
+    var topColor: Color
+    var bottomColor: Color
+    
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [topColor, bottomColor]),
+                       startPoint: .topLeading, endPoint: .bottomTrailing)
+        .ignoresSafeArea(.all)
+    }
+}
+
+#Preview {
+    BackgroundView(topColor: .red, bottomColor: .purple)
+}
