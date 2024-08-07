632b5b2e400ce3759375f328feaf2326ee45080f
Ramon Xavier <ramon1234rxm@gmail.com>
2024-08-06 22:51:51 -0300

    feat: create weather button view

---
+++ Weather/WeatherButton.swift
@@ -0,0 +1,27 @@
+//
+//  WeatherButton.swift
+//  Weather
+//
+//  Created by Ramon Xavier on 06/08/24.
+//
+
+import SwiftUI
+
+struct WeatherButton: View {
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    var body: some View {
+        Text(title)
+            .frame(width: 280, height: 50)
+            .background(backgroundColor)
+            .foregroundColor(textColor)
+            .font(.system(size: 20, weight: .bold, design: .default))
+            .cornerRadius(10)
+    }
+}
+
+#Preview {
+    WeatherButton(title: "Button", textColor: .white, backgroundColor: .red)
+}
