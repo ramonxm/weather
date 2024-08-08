308b9f7bdd343fe71adb1e3b1fe057833dcd189a
Ramon Xavier <ramon1234rxm@gmail.com>
2024-08-07 22:41:45 -0300

    feat: add is night state

--- Weather/WeatherAppView.swift
+++ Weather/WeatherAppView.swift
@@ -8,18 +8,21 @@
 import SwiftUI
 
 struct WeatherAppView: View {
+    @State private var isNight = false
+    
     var body: some View {
         ZStack {
-            BackgroundView(topColor: .blue, bottomColor: Color("lightblue"))
+            BackgroundView(topColor: isNight ? .black : .blue, bottomColor: isNight ? .gray : Color("lightblue"))
             VStack {
                 CityTextView(cityName: "Cupertino, CA")
-                WeatherStatusView(imageName: "cloud.sun.fill", temperature: 76)
+                WeatherStatusView(imageName: isNight ? "moon.stars.fill" : "cloud.sun.fill", temperature: 76)
                     .padding(.bottom, 40)
                 HStack(spacing: 20) {
                     WeatherDayView(dayOfWeek: "TUE", imageName: "cloud.sun.fill", temperature: 74)
                 }
                 Spacer()
                 Button {
+                    isNight.toggle()
                 } label: {
                     WeatherButton(title: "Change Day Time", textColor: .blue, backgroundColor: .white)
                 }
